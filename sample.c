d4a092245afc1541de8608733478a177f7dd8046
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-16 17:54:00 +0900

    else-if

--- sample.c
+++ sample.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-  int num;
-  scanf("%d", &num);
-  if(num == 10)
-    printf("10と同じです");
-  else
-    printf("10ではありません");
-
+  int age;
+  scanf("%d", &age);
+  if (age <= 3) {
+    printf("0円です");
+  } else if (age <= 20) {
+    printf("1000円です");
+  } else {
+    printf("2000円です");
+  }
   return 0;
 }
