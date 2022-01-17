be394ba9417f16102d7b9f7b3407ebb65affeeb3
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-17 20:12:12 +0900

    戻り値をif文に当てはめて応用

--- sample.c
+++ sample.c
@@ -3,8 +3,12 @@
 int fruits(int apple);
 int main (void){
     int hoge;
-    hoge = fruits(5);
-    printf("%d", hoge);
+    hoge = fruits(10);
+    if (hoge < 3000){
+        printf("予算に収まりました");
+    } else {
+        printf("予算オーバーです");
+    }
     return 0;
 }
 
