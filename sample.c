1575abd4e8da09db52f40a907c32705b143900d7
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-17 20:19:38 +0900

    ローカル変数
    変数はブロック内でしか使用出来ない。
    ブロックを超えると変数は寿命を迎えて死んでしまう。

--- sample.c
+++ sample.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
 
-int fruits(int apple);
 int main (void){
-    int hoge;
-    hoge = fruits(10);
-    if (hoge < 3000){
-        printf("予算に収まりました");
-    } else {
-        printf("予算オーバーです");
+    {
+      int apple = 30;
     }
+    printf("%d", apple);
     return 0;
-}
-
-int fruits(int apple){
-    int kingaku;
-    kingaku = apple * 300;
-    return kingaku;
 }
