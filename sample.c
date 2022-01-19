50a4ad976af4a5c17eb1551025bd5407ac5a3ad0
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-19 15:41:20 +0900

    sizeof演算子
    sizeof演算子を用いると、配列の要素数をわざわざ数えなくても勝手に算出してくれる

--- sample.c
+++ sample.c
@@ -3,7 +3,7 @@
 int main (void){
     int array [5] = {12, 13, 14};
     int i;
-    for(i = 0; i < 5; i++){
+    for(i = 0; i < sizeof(array) / sizeof(array[0]); i++){
         printf("%d番目は %d です\n", i, array[i]);
     }
     return 0;
