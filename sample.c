6a7d69a90e628513e0010ac7b8cde6d85a1ae74f
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-19 17:00:42 +0900

    strcpy関数を使用して文字を代入

--- sample.c
+++ sample.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main (void){
-    char str [] = "2.56";
-    double suuti = atoi (str);
-    printf("%f", suuti);
+    char str [10];
+    strcpy(str, "MIKE");
+    printf("%s", str);
     return 0;
 }
