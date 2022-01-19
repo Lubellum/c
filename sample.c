6002a3198f3eb2ac08df5883c0d9651851d1409d
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-19 15:38:50 +0900

    配列とfor文

--- sample.c
+++ sample.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main (void){
-    int array [] = {12, 13, 14};
-    printf("%d", array[1]);
+    int array [5] = {12, 13, 14};
+    int i;
+    for(i = 0; i < 5; i++){
+        printf("%d番目は %d です\n", i, array[i]);
+    }
     return 0;
 }
